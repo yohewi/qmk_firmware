072fbb57d73f6937a29e3e10395b571d05be0568
yohewi <yohewi@gmail.com>
2021-04-10 08:22:39 +0900

    更新

--- keyboards/kawaii/kawaii.h
+++ keyboards/kawaii/kawaii.h
@@ -25,39 +25,18 @@
  *
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
-
-#define LAYOUT( \
-    k00, k01, k02, \
-      k10,  k12    \
-) { \
-    { k00, k01,   k02 }, \
-    { k10, KC_NO, k12 }  \
-}
-
-#define LAYOUT( \
-    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, \
-    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, \
-    K20, K21, K22, K23, K24, K25, K26, K27, K28, \
-    K30, K31, K32, K33, K34, K35, K36, K37, K38 \
-) \
-{ \
-    { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09 }, \
-    { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19 }, \
-    { K20, K21, K22, K23, K24, K25, K26, K27, K28, KC_NO }, \
-    { K30, K31, K32, K33, K34, K35, K36, K37, K38, KC_NO } \
-}
  */
 
 #define LAYOUT( \
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, \
-    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, \
     K30, K31, K32, K33, K34, K35, K36, K37 \
 ) \
 { \
     { K00, K01, K02, K03, K04, K05, K06, K07, K08, K09 }, \
-    { K10, K11, K12, K13, K14, K15, K16, K17, K18, K19 }, \
-    { K20, K21, K22, K23, K24, K25, K26, K27, K28 }, \
-    { K30, K31, K32, K33, K34, K35, K36, K37 } \
+    { K10, K11, K12, K13, K14, K15, K16, K17, K18, KC_NO }, \
+    { K20, K21, K22, K23, K24, K25, K26, K27, K28, KC_NO }, \
+    { K30, K31, K32, K33, K34, K35, K36, K37, KC_NO, KC_NO } \
 }
 
