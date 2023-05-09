e1aaf994a95ca60d6d1c3a467013ac2a11b45bcb
yohewi <yohewi@gmail.com>
2023-05-10 00:21:57 +0900

    Add files via upload

--- keyboards/alicja/keymaps/default/keymap.c
+++ keyboards/alicja/keymaps/default/keymap.c
@@ -18,54 +18,52 @@
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _0,
-    _1
+    _Default,
+    _LOWER,
+    _RAISE,
+    _ADV
 };
 
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
-    QMKBEST = SAFE_RANGE,
-    QMKURL,
-    LOWER
+    Default,
+    LOWER,
+    RAISE,
+    ADV,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT( /* Base */
+    [_Default] = LAYOUT( /* Base */
 //,-----------------------------------------------------------------------------------------------------------.
    KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    JP_MINS, JP_CIRC, JP_YEN, KC_DEL, KC_ROPT,\
    KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT,   JP_LBRC, KC_BSPC, \
    KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    JP_SCLN, JP_COLN, JP_RBRC, KC_ENT, \
    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH, JP_UNDS, KC_RSFT, KC_UP, \
-   KC_LGUI,          KC_LALT, LT(1,KC_SPC), KC_BSPC, LT(1,KC_SPC),  KC_ENT, JP_ZKHK,             KC_LEFT, KC_DOWN, KC_RGHT
+   KC_LGUI,          KC_LALT, LT(LOWER,KC_SPC), KC_BSPC, LT(RAISE,KC_SPC),  KC_ENT, JP_ZKHK,          KC_LEFT, KC_DOWN, KC_RGHT
  ),
-    [1] = LAYOUT( /* extra */
+    [_LOWER] = LAYOUT( /* extra */
 //,---------------------------------------------------------------------------------------------------------------------------.
-   KC_PWR,   KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_INS, _______, KC_PWR, \
+   KC_PWR,   KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_INS, _______, _______, \
    _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______,  _______, _______, _______, \
    KC_CAPS, KC_VOLD, KC_VOLU, KC_MUTE, KC_PWR,  _______, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_PENT,  _______, _______, _______, \
    _______, _______, _______, _______, _______, _______, KC_PPLS, _______, KC_PMNS, KC_END,  KC_PGDN,  KC_PPLS, KC_PMNS, KC_END, \
+   _______,          _______, _______, _______, _______, _______, _______,           _______, _______, _______
+  ),
+    [_RAISE] = LAYOUT( /* extra */
+//,---------------------------------------------------------------------------------------------------------------------------.
+   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,\
+   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
    _______,          _______, _______, _______, _______, _______, _______,         _______, _______, _______
-  )
+  ),
+    [_ADV] = LAYOUT( /* extra */
+//,---------------------------------------------------------------------------------------------------------------------------.
+   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,\
+   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+   _______,          _______, _______, _______, _______, _______, _______,         _______, _______, _______
+   )
+    
 };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case QMKBEST:
-            if (record->event.pressed) {
-                // when keycode QMKBEST is pressed
-                SEND_STRING("QMK is the best thing ever!");
-            } else {
-                // when keycode QMKBEST is released
-            }
-            break;
-        case QMKURL:
-            if (record->event.pressed) {
-                // when keycode QMKURL is pressed
-                SEND_STRING("https://qmk.fm/\n");
-            } else {
-                // when keycode QMKURL is released
-            }
-            break;
-    }
-    return true;
-}
