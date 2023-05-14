99e0428b8145d07c12a526bd704cd2ed17e4b496
yohewi <yohewi@gmail.com>
2023-05-15 00:47:37 +0900

    Add files via upload

--- keyboards/alicja/keymaps/default/keymap.c
+++ keyboards/alicja/keymaps/default/keymap.c
@@ -35,7 +35,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_Default] = LAYOUT( /* Base */
 //,-----------------------------------------------------------------------------------------------------------.
-   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    JP_MINS, JP_CIRC, JP_YEN, KC_DEL, KC_ROPT,\
+   KC_ESC,   JP_1,   JP_2,    JP_3,    JP_4,    JP_5,    JP_6,    JP_7,    JP_8,    JP_9,    JP_0,    JP_MINS, JP_CIRC, JP_YEN, KC_DEL, KC_ROPT,\
    KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT,   JP_LBRC, KC_BSPC, \
    KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    JP_SCLN, JP_COLN, JP_RBRC, KC_ENT, \
    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_SLSH, JP_UNDS, KC_RSFT, KC_UP, \
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  ),
     [_LOWER] = LAYOUT( /* extra */
 //,---------------------------------------------------------------------------------------------------------------------------.
-   KC_PWR,   KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_INS, _______, _______, \
+   KC_PWR,   KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_INS, _______, RESET, \
    _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______,  _______, _______, _______, \
    KC_CAPS, KC_VOLD, KC_VOLU, KC_MUTE, KC_PWR,  _______, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_PENT,  _______, _______, _______, \
    _______, _______, _______, _______, _______, _______, KC_PPLS, _______, KC_PMNS, KC_END,  KC_PGDN,  KC_PPLS, KC_PMNS, KC_END, \
