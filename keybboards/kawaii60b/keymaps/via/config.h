958f79950101ea27aec1129297f3885ce23af8e9
yohewi <yohewi@gmail.com>
2025-01-04 09:31:41 +0900

    M0ii060試作機KaWaii60試作版のqmkファイル

---
+++ keybboards/kawaii60b/keymaps/via/config.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// place overrides here
+#define TAPPING_TERM 190
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+#define TAPPING_LAYER_TERM 140 // Custom LT Tapping term
+#define TAPPING_TERM_PER_KEY
+
+#ifdef MOUSEKEY_ENABLE
+  #undef MOUSEKEY_INTERVAL
+  #define MOUSEKEY_INTERVAL 1
+  #undef MOUSEKEY_TIME_TO_MAX
+  #define MOUSEKEY_TIME_TO_MAX 150
+
+  #undef MOUSEKEY_MAX_SPEED
+  #define MOUSEKEY_MAX_SPEED 3
+
+  #undef MOUSEKEY_MOVE_DELTA
+  #define MOUSEKEY_MOVE_DELTA 4
+
+  #undef MOUSEKEY_DELAY
+  #define MOUSEKEY_DELAY 0
+#endif
